9b3e3065a5727e3e47119b22fe0d85b7d7eedc8b
FaridehGhani <faridehghani98@gmail.com>
2022-09-26 20:58:27 +0330

    add errors

--- middle/errors.go
+++ middle/errors.go
@@ -1,1 +1,11 @@
 package middle
+
+import "errors"
+
+var (
+	ErrImageAlreadyExists   = errors.New("image already exists")
+	ErrChunkAlreadyExists   = errors.New("chunk already exists")
+	ErrImageNotFound        = errors.New("image not found")
+	ErrMalformedRequest     = errors.New("malformed request")
+	ErrUnsupportedMediaType = errors.New("unsupported payload format")
+)
